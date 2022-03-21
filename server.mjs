9b2b66d5b3ac255f14ca52f2054f35bbdb9212f0
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-03-21 23:05:57 +0100

    modified files for changes

--- server.mjs
+++ server.mjs
@@ -1,24 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import listings from './routes/listings.mjs';
-import morgan from 'morgan';
-import connectDB from './db.mjs'
 
 dotenv.config({ path: 'config/config.env' });
-
-//connect to mongodb server
-connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Mount routes
-
-
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
-}
-app.use('/api/v1/listings', listings);
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
 });
