ee56ed3ba113bc065800a4b048240ebbf43434fe
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-03-26 10:07:08 +0100

    updated server

--- server.mjs
+++ server.mjs
@@ -1,10 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './db.mjs';
+import listings from './routes/listings.mjs'
+import errorHandler from "./middlewares/errorHandler.mjs";
 
 dotenv.config({ path: 'config/config.env' });
 
+
+
+connectDB()
+
 const app = express();
-const port = process.env.PORT || 5000;
+
+//body parser
+app.use(express.json())
+
+app.use('/api/v1/listings', listings)
+
+// error handling middleware
+app.use(errorHandler)
+
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
