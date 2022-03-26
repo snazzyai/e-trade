87e35fba46220af0fac3bf9ce2e0dcace61fdc08
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-03-26 10:05:18 +0100

    added guard for db connection

--- db.mjs
+++ db.mjs
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGODB_URL)
-    console.log(`Mongodb connected: ${conn.connection.host}`)
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL)
+        console.log(`Mongodb connected: ${conn.connection.host}`)
+    }
+    catch(err){
+        console.log(err)
+    }
+
 }
 
 export default connectDB;
