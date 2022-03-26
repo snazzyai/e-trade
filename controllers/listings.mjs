50a8cfe35b43d6df39c82a0d2123b7ab7e828497
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-03-26 10:06:56 +0100

    updated listings controllers

--- controllers/listings.mjs
+++ controllers/listings.mjs
@@ -1,4 +1,6 @@
-/* 
+import Listing from "../models/Listing.mjs";
+
+/*
     @desc Get all listings
     @route GET /api/v1/listings
     @access Public
@@ -7,25 +9,43 @@ export const getListings = (req, res, next) => {
     res.status(200).json({ success: true, message: 'get all listings' });
 }
 
-/* 
+/*
     @desc Get one listing
     @route GET /api/v1/listings/:id
     @access Public
 */
-export const getListing = (req, res, next) => {
-    res.status(200).json({ success: true, message: `get listing from id ${req.params.id}` });
+export const getListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id)
+        if(!listing){
+            return res.status(401).json({ success: false , message:'listing not found'})
+        }
+
+        return res.status(200).json({success: true , message: listing })
+
+    } catch(err){
+        next(err)
+    }
 }
 
-/* 
+/*
     @desc Create new listing
     @route POST /api/v1/listings
     @access Private
 */
-export const createListing = (req, res, next) => {
-    res.status(200).json({ success: true, message: `created new listing` });
+export const createListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.create(req.body)
+        if(!listing){
+            return res.status(400).json({message: "unsuccessful"})
+        }
+        return res.status(201).json({ message: "success", data: listing })
+    }catch(err){
+        next(err)
+    }
 }
 
-/* 
+/*
     @desc Update a listing
     @route PUT /api/v1/listings/:id
     @access Private
@@ -34,7 +54,7 @@ export const updateListing = (req, res, next) => {
     res.status(200).json({ success: true, message: `update listing from id ${req.params.id}` });
 }
 
-/* 
+/*
     @desc Delete a listing
     @route PUT /api/v1/listings/:id
     @access Private
