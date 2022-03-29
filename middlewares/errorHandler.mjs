66b7c51a7299ce9a6361afbcb2cbb05f13f22097
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-03-30 00:57:53 +0200

    created error handler middleware

--- middlewares/errorHandler.mjs
+++ middlewares/errorHandler.mjs
@@ -1,7 +1,31 @@
+import ErrorResponse from "../utils/errorResponse.mjs";
+
 const errorHandler = (err, req, res, next) => {
 
-    console.log(err.stack.red)
-    return res.status(500).json({ success: false, error: err.message})
+
+    let error = { ...err }
+
+    // Mongoose Listing Errors
+    // duplicate key error
+    if (error.code === 11000){
+        const message = `Duplicate key found with id of ${error.value}`
+        error = new ErrorResponse(message, 400 )
+    }
+    // casting error
+    if (error.name === 'CastError'){
+        const message = `Error finding listing with id of ${error.value}`
+        error = new ErrorResponse(message, 404)
+    }
+
+    //validation error
+    //need to make it work
+    if(error.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400)
+    }
+
+
+    return res.status(err.statusCode || 500).json({ success: false, error: err.message || 'Server error'})
 
 }
 
