f1244d6c744e88722e57b5c5fc992991223fef7f
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-06-14 10:41:26 +0200

    added routes and cookies

--- server.mjs
+++ server.mjs
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser'
 import connectDB from './db.mjs';
 import listings from './routes/listings.mjs'
+import auth from "./routes/auth.mjs";
 import errorHandler from "./middlewares/errorHandler.mjs";
 
+
 dotenv.config({ path: 'config/config.env' });
-
-
 
 connectDB()
 
@@ -15,7 +16,12 @@ const app = express();
 //body parser
 app.use(express.json())
 
+//use cookie parser
+app.use(cookieParser())
+
+//Mount the routers
 app.use('/api/v1/listings', listings)
+app.use('/api/v1/auth', auth)
 
 // error handling middleware
 app.use(errorHandler)
