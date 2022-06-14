c39080ba22ff40895bc4362f89d2a9c84428508d
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-06-14 10:31:56 +0200

    added authguard to listings route

--- routes/listings.mjs
+++ routes/listings.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getListings, getListing, createListing, updateListing, deleteListing } from '../controllers/listings.mjs'
+import authGuard from "../middlewares/auth.mjs";
 
 const router = express.Router();
 
-router.route('/').get(getListings).post(createListing)
-router.route('/:id').get(getListing).put(updateListing).delete(deleteListing)
+router.route('/').get(getListings).post(authGuard, createListing)
+router.route('/:id').get(getListing).put(authGuard, updateListing).delete(authGuard, deleteListing)
 
 export default router;
